f8b70ebd70a985907fe183aa1b8223abf9477eeb
zachliu <zqliu643@outlook.com>
2022-04-12 15:33:41 +0800

    MVCdemo1

---
+++ src/app2.js
@@ -0,0 +1,21 @@
+import './app2.css'
+import $ from 'jquery'
+
+const $tabBar = $("#app2 .tab-bar")
+const $tabContent = $("#app2 .tab-content")
+const localkey = 'app2.index'
+const index = localStorage.getItem(localkey) || 0
+$tabBar.on('click', 'li', (e) => {
+    const $li = $(e.currentTarget);
+
+    $li.addClass("selected")
+        .siblings().removeClass("selected")
+
+    const index = $li.index();
+    localStorage.setItem(localkey, index)
+    $tabContent.children().eq(index)
+        .addClass('active')
+        .siblings().removeClass('active')
+})
+
+$tabBar.children().eq(index).trigger('click')
